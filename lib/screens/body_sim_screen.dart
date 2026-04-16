1fd89c81fc438bfbd4402c3c6046ef65a053cb69
mahmoud abdelghani <mahmoudabdelghani0997@gmail.com>
2026-04-16 16:19:17 +0200

    Adding screen commit

---
+++ lib/screens/body_sim_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class BodySimScreen extends StatefulWidget {
+  const BodySimScreen({super.key});
+
+  @override
+  State<BodySimScreen> createState() => _BodySimScreenState();
+}
+
+class _BodySimScreenState extends State<BodySimScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
